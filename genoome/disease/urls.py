08f2bc64ae390b594efb3bc1f73091cf58f307bf
Kamil Niski <kamil.niski@gmail.com>
2015-08-20 13:58:01 +0200

    Fix some modals (signin upload genome etc)

--- genoome/disease/urls.py
+++ genoome/disease/urls.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse_lazy
 
 from . import views
 
 urlpatterns = [
-    url(r'^upload/$', login_required(views.UploadGenome.as_view(), login_url='/accounts/signup/'), name='upload_genome'),
-    url(r'^browse/$', login_required(views.DisplayGenomeResult.as_view(), login_url='/accounts/signin/'), name='browse_genome'),
+    url(r'^upload/$', login_required(views.UploadGenome.as_view(), login_url=reverse_lazy('accounts:signin')), name='upload_genome'),
+    url(r'^browse/$', login_required(views.DisplayGenomeResult.as_view(), login_url=reverse_lazy('accounts:signin')), name='browse_genome'),
     # url(r'^view/$', views.special_case_2003),
 ]
