aaa3764e476e29e0579fd27611b5c42c6bda1e61
Kamil Niski <kamil.niski@gmail.com>
2015-08-20 14:42:37 +0200

    Fix upload button

--- genoome/disease/urls.py
+++ genoome/disease/urls.py
@@ -7,7 +7,7 @@ from django.core.urlresolvers import reverse_lazy
 from . import views
 
 urlpatterns = [
-    url(r'^upload/$', login_required(views.UploadGenome.as_view(), login_url=reverse_lazy('accounts:signin')), name='upload_genome'),
+    url(r'^upload/$', login_required(views.UploadGenome.as_view(), login_url=reverse_lazy('accounts:signup')), name='upload_genome'),
     url(r'^browse/$', login_required(views.DisplayGenomeResult.as_view(), login_url=reverse_lazy('accounts:signin')), name='browse_genome'),
     # url(r'^view/$', views.special_case_2003),
 ]
