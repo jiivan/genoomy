3bed9eb0843522db766697bd385b5c6577c22e01
Kamil Niski <kamil.niski@gmail.com>
2015-08-31 22:10:24 +0200

    Analyze data order in admin

--- genoome/disease/admin.py
+++ genoome/disease/admin.py
@@ -7,11 +7,17 @@ class SNPMarkerAdmin(admin.ModelAdmin):
     search_fields = ('rsid', 'risk_allele', 'p_value', 'or_or_beta', 'disease_trait')
     ordering = ('rsid',)
 
+
 class AlleleColorAdmin(admin.ModelAdmin):
     list_display = ('allele', 'color', 'snp_marker')
     raw_id_fields = ("snp_marker",)
 
 
+class AnalyzeDataOrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'uploaded_filename', 'paid')
+    search_fields = ('user', 'uploaded_filename')
+
+
 admin.site.register(SNPMarker, SNPMarkerAdmin)
 admin.site.register(AlleleColor, AlleleColorAdmin)
-admin.site.register(AnalyzeDataOrder)
+admin.site.register(AnalyzeDataOrder, AnalyzeDataOrderAdmin)
