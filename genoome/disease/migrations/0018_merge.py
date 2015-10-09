4d75f810414897acf7de0685707f91ffa3415da8
Kamil Niski <kamil.niski@gmail.com>
2015-10-09 15:07:15 +0200

    Merge migrations

---
+++ genoome/disease/migrations/0018_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('disease', '0017_remove_allelecolor_color'),
+        ('disease', '0017_snpmarkerarticle_gene_area'),
+    ]
+
+    operations = [
+    ]
