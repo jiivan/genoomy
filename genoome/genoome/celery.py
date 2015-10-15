ae7319002bdf1163faba9e416f747fdbddce293a
Kamil Niski <kamil.niski@gmail.com>
2015-10-15 11:38:35 +0200

    Change settings in celery and wsgi files

--- genoome/genoome/celery.py
+++ genoome/genoome/celery.py
@@ -5,7 +5,7 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'genoome.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'genoome.settings.development')
 
 from django.conf import settings
 
