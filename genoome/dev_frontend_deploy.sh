d241c1ea982a55686473000dad518e3e69b617ef
Kamil Niski <kamil.niski@gmail.com>
2015-10-19 15:13:28 +0200

    Gulp added to dev deployscript

--- genoome/dev_frontend_deploy.sh
+++ genoome/dev_frontend_deploy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-PYPATH=/home/ubuntu/.pyenv/versions/dev_venv/bin/python;
+VENVPATH=/home/ubuntu/.pyenv/versions/dev_venv
+PYPATH=$VENVPATH/bin/python;
+GULPPATH=$VENCPATH/bin/gulp;
 
-cd /opt/dev_genoome/genoome/genoome/ && sudo git checkout -- . && git pull origin dev && $PYPATH ./manage.py collectstatic --noinput --settings=genoome.settings.development && sudo touch /etc/uwsgi/vassals/dev_genoome.ini && sudo chown -R ubuntu:www-data /opt/dev_genoome/genoome/genoome/assets/
+cd /opt/dev_genoome/genoome/genoome/ && sudo git checkout -- . && \
+git pull origin dev && \
+$GULPPATH ../gulpfile.js dist:css dist:js &&
+$PYPATH ./manage.py collectstatic --noinput --settings=genoome.settings.development && \
+sudo touch /etc/uwsgi/vassals/dev_genoome.ini && \
+sudo chown -R ubuntu:www-data /opt/dev_genoome/genoome/genoome/assets/
 
 echo 'Success';
