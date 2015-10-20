c28c11d58f0de7e3ca2cf871756da42365746e27
Kamil Niski <kamil.niski@gmail.com>
2015-10-20 15:06:28 +0200

    dev deploy further fixes, pyenv problems

--- genoome/dev_frontend_deploy.sh
+++ genoome/dev_frontend_deploy.sh
@@ -11,7 +11,7 @@ eval "$(pyenv init -)";
 /home/ubuntu/.pyenv/bin/pyenv activate dev_venv;
 cd /opt/dev_genoome/genoome/genoome/ && sudo git checkout -- . && \
 git pull origin dev && \
-$GULPPATH ../gulpfile.js dist:css dist:js &&
+$GULPPATH ../gulpfile.js dist:css dist:js && \
 $PYPATH ./manage.py collectstatic --noinput --settings=genoome.settings.development && \
 sudo touch /etc/uwsgi/vassals/dev_genoome.ini && \
 sudo chown -R ubuntu:www-data /opt/dev_genoome/genoome/genoome/assets/;
