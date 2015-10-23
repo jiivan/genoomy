996c05aa5484258ff3797b8e10540068d0c8e70d
Kamil Niski <kamil.niski@gmail.com>
2015-10-23 15:55:42 +0200

    tags show on landing

--- genoome/accounts/views.py
+++ genoome/accounts/views.py
@@ -107,6 +107,6 @@ class LandingView(TemplateView):
     def get_context_data(self, **kwargs):
         sample_data_filepath = 'disease/{}'.format(self.sample_data_filename)
         kwargs['table'] = get_genome_data(sample_data_filepath)
-        kwargs['allele_tags'] = CustomizedTag.objects.all()
+        kwargs['allele_tags'] = CustomizedTag.objects.filter(show_on_landing=False)
         kwargs['legend_rows'] = get_legend_rows()
         return super().get_context_data(**kwargs)
