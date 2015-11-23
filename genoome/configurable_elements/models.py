56b8905a0b9df07dc4aea523eb5a97051c5d48f9
Kamil Niski <kamil.niski@gmail.com>
2015-11-23 18:09:50 +0100

    Legend rows by prioty

--- genoome/configurable_elements/models.py
+++ genoome/configurable_elements/models.py
@@ -6,6 +6,10 @@ from color_aliases.models import ColorAlias
 class LegendRow(models.Model):
     content = models.CharField(max_length=256)
     color = models.ForeignKey(ColorAlias)
+    priority = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['-priority']
 
     def __str__(self):
         return self.content
