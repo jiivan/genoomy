0914407d76db739563cf4e89490599b0b4578b1f
jvn <jiivan@o2.pl>
2015-12-21 12:06:34 +0100

    TTag dla disease data_table

---
+++ genoome/disease/templatetags/disease_tags.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from django import template
+from django.core.urlresolvers import reverse_lazy
+
+from configurable_elements.models import get_legend_rows
+from disease.models import CustomizedTag
+
+register = template.Library()
+
+
+@register.inclusion_tag('includes/data_table.html', takes_context=True)
+def data_table(context):
+    inner_context = {}
+    inner_context['legend_rows'] = get_legend_rows()
+    inner_context['allele_tags'] = CustomizedTag.objects.filter(show_on_data=True)
+    inner_context['genome_data_url'] = reverse_lazy('disease:landing_json_data')
+    return inner_context
