dcb490a0361ace4f24391d380dfbfee13c1114a7
jvn <jiivan@o2.pl>
2015-12-23 14:13:55 +0100

    W advanced dodac guzic domyslny

--- genoome/static/js/genoome.js
+++ genoome/static/js/genoome.js
@@ -89,7 +89,6 @@ var reset_checkboxes = function(checkbox_settings) {
     var checkboxes = $(".checkbox-row input");
     checkbox_settings.forEach(function(element, index) {
         var checkbox = $(checkboxes[index]);
-        console.log('trigger: %o %o->%o', index, checkboxes[index], element);
         checkbox.prop('checked', element).trigger('change');
     });
 
