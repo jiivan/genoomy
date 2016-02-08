784e674ed02e1d522d95ebc105a1eee4930e3a31
jvn <jiivan@o2.pl>
2016-02-08 15:41:23 +0100

    23andme api WIP

---
+++ genoome/twentythree/models.py
@@ -0,0 +1,108 @@
+from django.conf import settings
+from django.db import models
+import logging
+
+log = logging.getLogger('genoome.twentythree.models')
+
+class Token23(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, unique=True)
+    access_token = models.TextField()
+    refresh_token = models.TextField()
+    scope = models.TextField()
+    created_at = models.DateTimeField(auto_add_now=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def _api_get(self, url):
+        headers = {
+            'Authorization': 'Bearer %s' % self.access_token,
+        }
+        response = requests.get(url, headers=headers)
+        if response.status_code != 200:
+            # https://api.23andme.com/docs/errors/ 
+            log.warning('23andme error response: %s\nurl:%s', response.text, url)
+            raise self.ClientError
+        return response.json()
+
+    @classmethod
+    def get_by_code(klass, user, code):
+        # https://api.23andme.com/docs/authentication/ 
+        # curl https://api.23andme.com/token/
+        #       -d client_id=xxx \
+        #       -d client_secret=yyy \
+        #       -d grant_type=authorization_code \
+        #       -d code=zzz \
+        #       -d "redirect_uri=https://localhost:5000/receive_code/"
+        #       -d "scope=basic%20rs3094315"
+        post_data = {
+            'client_id': settings.CLIENT_ID23,
+            'client_secret': settings.CLIENT_SECRET23,
+            'scope': 'basic genomes',
+            'code': code,
+            'grant_type': 'authorization_code',
+            'redirect_uri': settings.COMEBACK_URL23,
+        }
+        response = requests.post('/token/', data=post_data, timeout=30.00, verify=True)$
+        if response.status_code != 200:$
+            log.error('Problem fetching token %', response.status_code, response.text)$
+            raise self.ClientError$
+        data = response.json()$
+        return klass.objects.create(user=user, access_token=data['access_token'], refresh_token=data['refresh_token'], scope=data['scope'])
+
+    def refresh(self):
+        post_data = {
+            'client_id': settings.CLIENT_ID23,
+            'client_secret': settings.CLIENT_SECRET23,
+            'scope': self.scope,
+            'refresh_token': self.refresh_token,
+            'grant_type': 'refresh_token',
+            'redirect_uri': settings.COMEBACK_URL23,
+        }
+        response = requests.post('/token/', data=post_data, timeout=30.00, verify=True)$
+        if response.status_code != 200:$
+            log.error('Problem refreshing token %', response.status_code, response.text)$
+            raise self.ClientError$
+        data = response.json()$
+        self.access_token = data['access_token']
+        self.refresh_token = data['refresh_token']
+        self.scope = data['scope']
+        self.save()
+
+    def get_genome(self, profile_id23):
+        # GET /1/genomes/profile_id/?unfiltered=...
+        # curl https://api.23andme.com/1/genomes/c44.../ -H "..."
+        # https://api.23andme.com/res/txt/snps.b4e00fe1db50.data 
+        # scope required: genomes
+        data = self._api_get('/1/genomes/%s/' % (profile_id23,))
+        return data['genome']
+
+    def get_profiles(self):
+        # GET /1/user/
+        # # JSON response:
+        #{
+        #    "id": "a42e94634e3f7683",
+        #    "profiles": [
+        #        {
+        #            "genotyped": true,
+        #            "id": "c4480ba411939067"
+        #        }, ...
+        #    ]
+        #}
+        # scope required: basic
+
+        data = self._api_get('/1/user/')
+        return data['profiles']
+
+
+class CeleryTask23(models.Model):
+    STATUS_CHOICES = (
+        ('new', 'new'),
+        ('fetching', 'fetching genome'),
+        ('parsing', 'parsing genome'),
+        ('error', 'error'),
+    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, unique=True)
+    chosen_profile = models.TextField()
+    fetch_task_id = models.TextField()
+    analyze_order = models.ForeignKey('disease.models.AnalyzeDataOrder', null=True)
+    process_task_id = models.TextField(null=True)
+    status = models.TextField(choices=STATUS_CHOICES, default='new')
---
+++ genoome/twentythree/views.py
@@ -0,0 +1,77 @@
+from django.contrib.auth.decorators import login_required
+from django.core.cache import cache
+from django.http import HttpResponseRedirect
+from django.views.generic.edit import FormView
+import logging
+import urllib.parse
+
+from twentythree.forms import ChooseProfileForm
+
+log = logging.getLogger('genoome.twentythree.views')
+
+def token_required(f):
+    def inner(request, *args, **kwargs):
+        has_token = request.user.token23_set.exists()
+        if not has_token:
+            return HttpResponseRedirect(reverse_lazy('23andme:login'))
+        return f(request, *args, **kwargs)
+    return inner
+
+@login_required
+def login23(request):
+    # https://api.23andme.com/docs/authentication/ 
+    url = 'https://api.23andme.com/authorize/?redirect_uri=%s&response_type=code&client_id=%s&scope=basic%20genomes'
+    url %= (
+        urllib.parse.quote(settings.COMEBACK_URL23),
+        settings.CLIENT_ID23,
+    )
+    return HttpResponseRedirect(url)
+
+def comeback(request):
+    try:
+        code = request.GET['code']
+    except KeyError:
+        log.info('No code. Redirecting to 23andme_login(%s)', request.user)
+        return HttpResponseRedirect(reverse_lazy('23andme:login'))
+    try:
+        token = Token.get_by_code(request.user, code)
+    except Token.ClientError:
+        log.error('Failed token(%r). Redirecting to 23andme_login(%s)', code, request.user)
+    return HttpResponseRedirect(reverse_lazy('23andme:profiles'))
+
+class ChooseProfileView(FormView):
+    form_class = ChooseProfileForm
+    success_url = reverse_lazy('23andme:status')
+    template_name = 'twentythree/choose_profile.html'
+
+    def get(self, request, *args, **kwargs):
+        self.get_token().get_profiles()
+        genotyped_profiles = [p for p in profiles if p['genotyped']]
+        if len(genotyped_profiles) == 1:
+            # use this profile
+            form = self.get_form()(self.get_form_kwargs())
+            return self.form_valid(form)
+        return super().get(request, *args, **kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = self.get_form_kwargs()
+        kwargs['profiles'] = self.get_token().get_profiles()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+profiles = login_required(token_required(ChooseProfileView.as_view()))
+
+@login_required
+@token_required
+def status(request):
+    try:
+        ctask = CeleryTask23.objects.filter(user=request.user).order_by('-pk')[0]
+    except CeleryTask23.DoesNotExist:
+        return HttpResponseRedirect(raverse_lazy('23andme:profiles'))
+    if ctask.status = 'parsing':
+        return HttpResponseRedirect(genomeurl)
+    return render_to_response('twentythree/status.html', {'ctask': ctask})
